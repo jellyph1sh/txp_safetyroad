883eb5b7cc2f53077431fc9e7512a928793f801f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 08:56:44 +0200

    :heavy_plus_sign: | Add NativeUILua_Reloaded resource to dependencies.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,10 @@ version "1.0"
 description "Script with a menu to create a slowdown zone and set signs."
 author "Time_XP"
 
-client_script "client/cl_safetyroad.lua"
+client_scripts {
+    "@NativeUILua_Reloaded/src/NativeUIReloaded.lua",
+    "client/cl_safetyroad.lua"
+}
 
 server_script "server/sv_safetyroad.lua"
 
