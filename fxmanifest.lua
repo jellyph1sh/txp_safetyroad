e9455cf23720d0782c236caecaee2747192ee787
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 08:51:25 +0200

    :rocket: | Setup the script structure.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "Script with a menu to create a slowdown zone and set signs."
+author "Time_XP"
+
+client_script "client/cl_safetyroad.lua"
+
+server_script "server/sv_safetyroad.lua"
+
+shared_script "shared/config.lua"
